3d28a78a1dcd3e2839a8fb2fb712b6502eaeb233
hallowlab <oscarcastillo00824@gmail.com>
2024-07-07 18:35:19 -0400

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// sendFile will go heres
+// sendFile will go here
 
 app.use(express.static(__dirname + "/src/public"));
 
