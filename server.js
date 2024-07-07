7bd4386aaaec44305f4dc38031425fc276f0c8ef
hallowlab <oscarcastillo00824@gmail.com>
2024-07-07 18:32:59 -0400

    update

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go heres
+
+app.use(express.static(__dirname + "/src/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/src/index.html"));
+});
+
+app.get("/raids", function (req, res) {
+  res.sendFile(path.join(__dirname, "/src/raids.html"));
+});
+
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
